da5cd6037b3a6e851db239b5c2a44bc1499752e4
xgl <34029067@qq.com>
2022-06-05 11:38:48 +0800

    第十五次提交，基于log4j日志技术收集异常，控制日志信息输送的目的地是控制台、文件等位置

--- src/main/java/com/heima/travel/advice/ProjectExceptionAdvice.java
+++ src/main/java/com/heima/travel/advice/ProjectExceptionAdvice.java
@@ -5,6 +5,8 @@ import com.heima.travel.exception.SystemException;
 import com.heima.travel.vo.Code;
 import com.heima.travel.vo.ResultInfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,11 +25,19 @@ import java.io.IOException;
 //ControllerAdvice注解: 添加在异常处理器上
 @RestControllerAdvice
 public class ProjectExceptionAdvice {
-
+    //0.添加pom.xml依赖
+    //1.获取日志对象 绑定当前的类
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProjectExceptionAdvice.class);
     //专门处理BusinessException
     @ExceptionHandler(BusinessException.class)
     public ResultInfo doBusinessException(BusinessException ex, HttpServletResponse response, HttpServletRequest request) throws IOException {
-//        System.out.println("异常触发");
+        //TODO：3. log4j打印日志-->在本地生成日志文件
+        // 4.获取用户当前请求的url地址，定位报错信息的请求路径
+        String requestURI = request.getRequestURI();
+        // 5.获取错误信息
+        String errorMsg = ex.getMessage();
+        // 6.设置日志输出级别
+        LOGGER.info("用户访问：{},报错：{}",requestURI,errorMsg);
         response.sendRedirect("/error.html");
         //发送对应消息传递给用户，提醒规范操作
         return new ResultInfo(false,ex.getCode(),ex.getMessage());
@@ -37,6 +47,13 @@ public class ProjectExceptionAdvice {
     @ExceptionHandler({SystemException.class})
     public ResultInfo doSystemException(SystemException ex, HttpServletResponse response, HttpServletRequest request) throws IOException {
         response.sendRedirect("/error.html");
+        //TODO：3. log4j打印日志-->在本地生成日志文件
+        // 4.获取用户当前请求的url地址，定位报错信息的请求路径
+        String requestURI = request.getRequestURI();
+        // 5.获取错误信息
+        String errorMsg = ex.getMessage();
+        // 6.设置日志输出级别
+        LOGGER.info("用户访问：{},报错：{}",requestURI,errorMsg);
 //        发送固定消息传递给用户，安抚用户
 //        发送特定消息给运维人员，提醒维护
 //                记录日志
